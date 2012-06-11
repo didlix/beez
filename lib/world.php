2533697d82772a6fad5398044cdb76477863085c
Rachel Graves <rachel@didlix.co.uk>
2012-06-11 20:49:55 +0100

    The game world

---
+++ lib/world.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * The game object, knows the rules and acts as a factory for artifacts
+ *
+ * @package default
+ * @author Rachel Graves
+ **/
+class game {
+	
+	public function __construct() {
+		
+	}
+
+
+}
