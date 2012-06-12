958d59b018ebba4375c1d5524e2288535ef9e352
Rachel Graves <rachel@didlix.co.uk>
2012-06-12 11:40:35 +0100

    Added rule to see if all bees are dead, ends the game if all bees are dead

---
+++ lib/Rule_All_Bees_Die.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once('Rule.php');
+
+
+/**
+ * If all Queen bees die then the game should end
+ */
+class Rule_All_Bees_Die extends Rule {
+
+	/**
+	 * Cound the number of Queen Bee Artifacts in the world
+	 */
+	public function rule(World $world)
+	{
+
+		$queen_bees = 0;
+
+		foreach ($world->getArtifacts() AS $artifact) {
+
+			if('Queen_Bee' === get_class($artifact)) {
+				if(false === $artifact->isDead()) {
+					$queen_bees++;
+			}
+
+		}
+
+		if($queen_bees <= 0) {
+			$world->gameOver('All Queen Bees Are Dead');
+		}
+
+	}
+
+}
