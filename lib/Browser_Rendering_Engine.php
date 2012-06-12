d16d06a54e08f5e03bde557ec4e32200a572ec56
Rachel Graves <rachel@didlix.co.uk>
2012-06-12 11:42:57 +0100

    Output game over message if game is over, otherwise, output the artifacts

--- lib/Browser_Rendering_Engine.php
+++ lib/Browser_Rendering_Engine.php
@@ -18,35 +18,65 @@ class Browser_Rendering_Engine extends Rendering_Engine
 	 * @author Rachel Graves
 	 * @param World $world
 	 **/
-	public function render(World $world)
+	public function render(World $world, $data = null)
 	{
+
+		if(null !== $data) {
+			$serialized_game = $data;
+		}
 
 		$output[] = '<html>';
 		$output[] = '<head><title>Beez Game</title></head>';
 		$output[] = '<body>';
-		
-		$artifacts = $world->getArtifacts();
 
-		foreach ($artifacts AS $artifact) {
-			$id = $artifact->getId();
+		// Only output if the game is still going	
+		if(false === $world->isGameOver()) {
 
-			$output[$id] = '<div class="artifact">';
-			$output[$id].= '<span class="name">' . $artifact->getWorldName() . '</span>';
-			$output[$id].= '<span class="hit-points">' . $artifact->getHitPoints() . '</span>';
+			$artifacts = $world->getArtifacts();
 
-			if($artifact->isDead()) { 
+			foreach ($artifacts AS $artifact) {
+				$id = $artifact->getId();
 
-				$output[$id].= '<span class="dead">Dead</span>';
+				$output[$id] = '<div class="artifact">';
+				$output[$id].= '<span class="name">' . $artifact->getWorldName() . '</span>';
+				$output[$id].= '<span class="hit-points">' . $artifact->getHitPoints() . '</span>';
 
+				if($artifact->isDead()) { 
+
+					$output[$id].= '<span class="dead">Dead</span>';
+
+				}
+
+				$output[$id].= '</div>';
 			}
+		} else {
 
-			$output[$id].= '</div>';
+			$output[] = '<div>' . $world->getGameOverMessage() . '</div>';
 		}
+
+		// @todo form
+
+		$output[] = '<form method="POST" action="index.php">';
+
+		$output[] = '<input type="submit" name="Hit" id="turn" value="Hit" />';
+		$output[] = '<input type="submit" name="start" id="start" value="Start A New Game" />';
+		
+		if(isset($serialized_game)) {
+			$output[] = '<input type="hidden" value="' . $serialized_game . '" />';
+		}
+
+		$output[] = '</form>';
 
 		$output[] = '</body>';
 		$output[] = '</html>';
 
-		return $output;
+		$html = '';
+
+		foreach ($output AS $html_line) {
+			$html .= $html_line . PHP_EOL;
+		}
+
+		return $html;
 
 	}
 
