b2ba07fb3575cc2b4242776bc96c1690b9ac054c
Rachel Graves <rachel@didlix.co.uk>
2012-06-12 11:19:02 +0100

    Return HTML from Browser Renderer

--- lib/Browser_Rendering_Engine.php
+++ lib/Browser_Rendering_Engine.php
@@ -11,9 +11,43 @@ require_once('Rendering_Engine.php');
 class Browser_Rendering_Engine extends Rendering_Engine 
 {
 
-	public function render()
+	/**
+	 * Return HTML output, takes World object
+	 *
+	 * @return string
+	 * @author Rachel Graves
+	 * @param World $world
+	 **/
+	public function render(World $world)
 	{
-		echo "Bees:";
+
+		$output[] = '<html>';
+		$output[] = '<head><title>Beez Game</title></head>';
+		$output[] = '<body>';
+		
+		$artifacts = $world->getArtifacts();
+
+		foreach ($artifacts AS $artifact) {
+			$id = $artifact->getId();
+
+			$output[$id] = '<div class="artifact">';
+			$output[$id].= '<span class="name">' . $artifact->getWorldName() . '</span>';
+			$output[$id].= '<span class="hit-points">' . $artifact->getHitPoints() . '</span>';
+
+			if($artifact->isDead()) { 
+
+				$output[$id].= '<span class="dead">Dead</span>';
+
+			}
+
+			$output[$id].= '</div>';
+		}
+
+		$output[] = '</body>';
+		$output[] = '</html>';
+
+		return $output;
+
 	}
 
 } // END class Browser_Renderer 
