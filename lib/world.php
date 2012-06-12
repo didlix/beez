ba23299632bd05f0ab0e8a9bec1de22a36f1a6f9
Rachel Graves <rachel@didlix.co.uk>
2012-06-12 07:48:53 +0100

    Renamed store method to store Artifact

--- lib/world.php
+++ lib/world.php
@@ -1,16 +1,40 @@
 <?php
 
 /**
- * The game object, knows the rules and acts as a factory for artifacts
+ * The world object, knows the rules and acts as a factory for artifacts
  *
  * @package default
  * @author Rachel Graves
  **/
-class game {
-	
-	public function __construct() {
-		
-	}
+abstract class World {
+    
+    /**
+     * undocumented class variable
+     *
+     * @var string
+     **/
+    var $_id;
+
+
+    /**
+     * Artifacts stored in this world
+     *
+     * @var string
+     **/
+    var $_artifacts = array();
+
+    /**
+     * store an artifact in the world
+     *
+     * @return void
+     * @author Rachel Graves
+     **/
+    public function store(Artifact $artifact)
+    {
+        $this->_artifacts[] = $artifact;
+    }
+
+
 
 
 }
