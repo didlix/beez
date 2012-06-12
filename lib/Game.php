f9bc0ef0e3b09296918b06a3b5e5dea42ec89a72
Rachel Graves <rachel@didlix.co.uk>
2012-06-12 10:15:01 +0100

    Added Game class

---
+++ lib/Game.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * The game engine which includes the world and its artifacts
+ *
+ * @package default
+ * @author Rachel Graves 
+ **/
+class Game
+{
+
+	/**
+	 * A world object
+	 *
+	 * @var World object
+	 **/
+	private $_world;
+
+	/**
+	 * Unique ID
+	 *
+	 * @var string
+	 **/
+	private $_id;
+
+	/**
+	 * Rendering Object
+	 *
+	 * @var Renderer object
+	 **/
+	private $_renderingEngine;
+
+	/**
+	 * Store a unique id, world object and rendering engine
+	 *
+	 * @return void
+	 * @author 
+	 **/
+	function __construct(Rendering_Engine $renderingEngine)
+	{
+		$this->_world = $world;
+		$this->_renderingEngine = $renderingEngine;
+        $this->_id = uniqid('game');
+	}
+
+} // END class Game 
